ea42de8787af31c942c84a7b4ec799e3352fff41
edwinx29 <edwinxkristian@gmail.com>
2018-07-24 09:52:20 +0700

    First upload

---
+++ cssjs.php
@@ -0,0 +1,37 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
+<!-- bootstrap -->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+
+<link href="themes/css/bootstrappage.css" rel="stylesheet"/>
+
+<!-- global styles -->
+<link href="themes/css/flexslider.css" rel="stylesheet"/>
+<link href="themes/css/main.css" rel="stylesheet"/>
+
+<!-- scripts -->
+<script src="themes/js/jquery-1.7.2.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="themes/js/superfish.js"></script>
+<script src="themes/js/jquery.scrolltotop.js"></script>
+
+<link rel="shortcut icon" href="">
+<link rel="apple-touch-icon" sizes="57x57" href="assets/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="assets/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="assets/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="assets/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="assets/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="assets/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="assets/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="assets/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="assets/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="assets/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="assets/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png">
+<link rel="manifest" href="assets/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="assets/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
